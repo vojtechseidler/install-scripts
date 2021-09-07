6c4fd04dc877ae07b871242e38b7d53f54231cd1
Vojtěch Seidler <mail@vojtech-seidler.cz>
2021-09-07 14:07:12 +0200

    Use opt folder for installation files

--- python-3.9.sh
+++ python-3.9.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+INSTALLATION_LOCATION="/opt/python3.9.7-installation"
+
 echo
 echo "Start install..."
 echo
 
 apt update
 apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+
+mkdir -p $INSTALLATION_LOCATION
+cd $INSTALLATION_LOCATION
 
 wget https://www.python.org/ftp/python/3.9.7/Python-3.9.7.tgz
 
@@ -19,9 +24,10 @@ make -j $(getconf _NPROCESSORS_ONLN)
 
 make altinstall
 
+cd $INSTALLATION_LOCATION
 cd ../
 
-rm -rf Python-3.9.7
+rm -rf $INSTALLATION_LOCATION
 
 python3.9 -m pip --upgrade pip
 python3.9 -m pip install virtualenv
